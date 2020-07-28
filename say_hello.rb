1821121ce6af77511a8c5363487701b2344a4d08
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2020-07-28 20:29:47 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,11 @@
 # Build your say_hello method here
 
-def say_hello(name="Ruby")
+def say_hello(name="Ruby Programmer")
   # puts "Hello #{name}!"
   puts "Hello " + name + "!"
 end
 
 # say_hello("Alex")
 # say_hello("Sashin")
+
 
